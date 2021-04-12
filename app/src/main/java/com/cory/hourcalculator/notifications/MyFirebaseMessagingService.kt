4c5a08d3ebd66892670b23d2b43c7f76d8d2fb69
corylowry12 <corylowry12@gmail.com>
2021-04-12 01:53:53 -0500

    Commented out notification function

--- app/src/main/java/com/cory/hourcalculator/notifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/cory/hourcalculator/notifications/MyFirebaseMessagingService.kt
@@ -1,8 +1,18 @@
 package com.cory.hourcalculator.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import com.cory.hourcalculator.R
+import com.cory.hourcalculator.activities.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -43,7 +53,7 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         Log.d("token", "sendRegistrationTokenToServer($p0)")
     }
 
-    /*fun sendNotification(messageBody: String) {
+   /* fun sendNotification(messageBody: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
