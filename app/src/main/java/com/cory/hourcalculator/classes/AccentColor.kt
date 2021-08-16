a2513221c6d71928c917b442bc700a20f2336daa
corylowry12 <corylowry12@gmail.com>
2021-08-15 22:46:00 -0500

    Reformatted code

--- app/src/main/java/com/cory/hourcalculator/classes/AccentColor.kt
+++ app/src/main/java/com/cory/hourcalculator/classes/AccentColor.kt
@@ -14,39 +14,33 @@ class AccentColor(context: Context) {
         editor.putInt("Accent", state)
         editor.apply()
     }
+
     // this will load the night mode state
-    fun loadAccent(): Int{
+    fun loadAccent(): Int {
         val state = sharedPreferences.getInt("Accent", 0)
         return (state)
     }
 
-    fun alertTheme(context: Context) : Int {
+    fun alertTheme(context: Context): Int {
         val state = sharedPreferences.getInt("Accent", 0)
 
         val darkThemeData = DarkThemeData(context)
 
         if (state == 0 && !darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleLight
-        }
-        else if (state == 0 && darkThemeData.loadDarkModeState()) {
+        } else if (state == 0 && darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleDark
-        }
-        else if (state == 1 && !darkThemeData.loadDarkModeState()) {
+        } else if (state == 1 && !darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleLight_pink
-        }
-        else if (state == 1 && darkThemeData.loadDarkModeState()) {
+        } else if (state == 1 && darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleDark_pink
-        }
-        else if (state == 2 && !darkThemeData.loadDarkModeState()) {
+        } else if (state == 2 && !darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleLight_orange
-        }
-        else if (state == 2 && darkThemeData.loadDarkModeState()) {
+        } else if (state == 2 && darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleDark_orange
-        }
-        else if (state == 3 && !darkThemeData.loadDarkModeState()) {
+        } else if (state == 3 && !darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleLight_red
-        }
-        else if (state == 3 && darkThemeData.loadDarkModeState()) {
+        } else if (state == 3 && darkThemeData.loadDarkModeState()) {
             return R.style.AlertDialogStyleDark_red
         }
 
